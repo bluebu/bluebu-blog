ce14c43d163d86eb6f272cc581ae58f386e70964
wang <wangdeyueer@gmail.com>
2010-11-29 10:28:41 +0800

    init project

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+gem 'mysql'
+
+gem 'ruby-debug'
+
+group :development, :test do
+  gem 'webrat'
+  gem 'mongrel'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluebu::Application.config.secret_token = '056dc5344daf2523581903c4e532648a1f34e895b2d60cbfcb642a7095b7cc95893077ea6f0f9c08ddf863cf882df0200d508f861266230cca7d9d9960f58a0b'
